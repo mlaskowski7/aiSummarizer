fa996ad48a688f27872be141de32de09c4e2541c
Mateusz Laskowski <mlaskowski7>
2024-01-05 23:30:00 +0100

    hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { logo } from '../assets';
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <header className="w-full flex justify-center flex-col">
+      <nav className="flex justify-between items-center flex-row w-full pt-3">
+
+        <img src={logo} alt="my logo" className='w-24 object-contain' />
+
+        <button type="button" onClick={() => window.open('https://github.com/mlaskowski7/aiSummarizer')} className='black_btn'>
+          GitHub Repo
+        </button>
+
+      </nav>
+
+      <h1 className="head_text">
+        Summarize Articles with <br className='max-md:hidden'/>  <span className='orange_gradient'>OpenAI's GPT-4</span>
+      </h1>
+ 
+    </header>
   )
 }
 
